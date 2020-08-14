b96190393ec339b19de5e07dd97a9161939474ff
NitinLondey <43016854+NitinLondey@users.noreply.github.com>
2020-08-13 19:06:05 -0500

    Added Search  bar and Jquery library tag

--- js/topmenu.js
+++ js/topmenu.js
@@ -10,8 +10,22 @@ let addTopMenu = function(){
                                 '<a href="https://www.albany.edu/myualbany">MYUALBANY</a>'+
                                 '<a href="https://www.albany.edu/apply-now">APPLY</a>'+
                                 '<a href="https://www.alumni.albany.edu/s/1642/18-giving/landing.aspx?sid=1642&gid=2&pgid=2040&appealcode=uahome">'+
-                                '   GIVE</a>'+    
+                                '   GIVE</a>'+ 
+                                '<button type="button" style="padding-left:25px;padding-right:15px;" class="btn1" id="search-toggle">'+
+                                '<span class="fa fa-search"></i>'+
+                                '</button>'+
+    
+                       
+                                '<span class="mainSearch" style="margin-right:-307px; display:inline-block;">'+
+                                '<button type="button" style="padding-left:25px;padding-right:15px;" class="btn1" id="times-button">'+
+                                '<span class="fa fa-times"></i>'+
+                                '</button>'+
+                               
+                                '<input  style="vertical-align:middle;" class="searchInput" id="textInput" type="text"><input class="submitButton" value="Search" type="submit" onclick="getValue()">'+
+                                '</span>'   +
                             '</div>'+
+
+                            
                         '</div>';
     navheader.innerHTML = headerContent;
 
@@ -68,7 +82,7 @@ let addTopMenu = function(){
                                             '<li class="listSub" style="margin-top: -5px;"><a class="linkforlist" href="https://www.albany.edu/its/">IT Services</a></li>'+
                                         '</ul>'+
                                     '</div>'+
-                                    '<div class="col-md-2.5 " style="margin-top: 2em;">'+
+                                    '<div class="col-md-1.5 " style="margin-top: 2em;">'+
                                         '<ul>'+
                                             '<li class="headingList"></li>'+
                                             '<li class="listSub" style="margin-top: -5px;"><a class="linkforlist" href="https://events.albany.edu/cal/main/showEventList.rdo;jsessionid=npziD_rxShBzkDjVTJfRzRIX5wtUC5BC3yieI27B.bede-svc-p101">Events</a></li>'+
@@ -125,20 +139,28 @@ addTopMenu();
 
 $('#buttonatag').click(function (e) {
 
+
     e.preventDefault(); // Prevent default Browser anchor-click behavior   
     if($(e.target).hasClass("fa") ){ 
         flag_fa=true;
-        $("#buttonatag").trigger('click');        
+        $("#buttonatag").trigger('click'); 
+       
     }
     else{
-        let innerHTML = this.innerHTML;
-        if (innerHTML.includes('MENU') == true) {
-            this.innerHTML = 'CLOSE<br><i  class="fa fa-times"></i>';
-        }
-        else {
-            this.innerHTML = 'MENU<br><i  class="fa fa-bars"></i>';
-        } 
-    }  
+
+       
+            let innerHTML = this.innerHTML;
+            if (innerHTML.includes('MENU') == true) {
+                this.innerHTML = 'CLOSE<br><i  class="fa fa-times"></i>';
+            }
+            else {
+                this.innerHTML = 'MENU<br><i  class="fa fa-bars"></i>';
+            }
+        
+    }
+    
+
+   
 });
 
 $(document).ready(function () {
@@ -149,6 +171,108 @@ $(document).ready(function () {
             $("#buttonatag").click();
         }
     });
+
+
+var flag=false;
+   /*  $('#search-toggle').click(function() {
+       //$('.mainSearch').removeClass('hidden');
+        $('.mainSearch').addClass('inlineDisplay');
+        $('.search-button').addClass('hidden');
+        $('#search-toggle').addClass('hidden');
+
+
+       //$('.mainSearch').toggle("slide", { direction: "left" }, 5000);
+
+
+        $('.mainSearch').show('slow');   
+            
+  
+      });
+     
+  
+      $('#times-button').click(function() {
+      //  $('.mainSearch').addClass('hidden');
+        //$('.mainSearch').removeClass('inlineDisplay');
+        $('.search-button').removeClass('hidden');
+        $('#search-toggle').removeClass('hidden');
+
+      //  $('.mainSearch').toggle("slide", { direction: "right" }, 1000);
+
+        $('.mainSearch').hide('slow');   
+
+      });
+ */
+
+
+$('#search-toggle').one('click', function() {
+
+    $(".mainSearch").animate({ "margin-right":  "0px" },500);    
+    flag=true;
 });
 
+
+
+
+$('#search-toggle').click(function() {
+   //$('.mainSearch').removeClass('hidden');
+   if(flag){
+    $('.mainSearch').addClass('inlineDisplay');
+    // $('.search-button').addClass('hidden');
+     $('#search-toggle').addClass('hidden');
+
+
+    //$('.mainSearch').toggle("slide", { direction: "left" }, 5000);
+
+   // $(".mainSearch").animate({marginRight: "-300px"}, 500 );
+   //  $('.mainSearch').toggle("slide");   
+     
+   $(".mainSearch").animate({ "margin-right":  "0px" },500); 
+   }
+   
+   });
+  
+
+   $('#times-button').click(function() {
+   //  $('.mainSearch').addClass('hidden');
+     //$('.mainSearch').removeClass('inlineDisplay');
+    // $('.search-button').removeClass('hidden');
+$('#search-toggle').removeClass('hidden');
+
+   //  $('.mainSearch').toggle("slide", { direction: "right" }, 1000);
+
+
+   $(".mainSearch").animate({ "margin-right":  "-307px" },500); 
+   
+
+   // $('.mainSearch').hide('slow');   
+
+   });
+
+
+
+
+     
+  
+     
+
+
+});
 $('#navbarSupportedContent').collapse('hide');
+
+
+
+function getValue() {
+    var text_input = document.getElementById('textInput').value;
+    console.log(text_input);
+  
+  
+  
+  
+  if(text_input === ""){
+      window.open("https://www.albany.edu/search/search_results.php?cx=009452333206896616693%3Aabbjmkl5yry&cof=FORID%3A11&ie=UTF-8&sa.x=0&sa.y=0&sa=Search&siteurl=www.albany.edu%2F&ref=www.google.com%2F&ss=233j24671j6&q=");
+  
+  }else{
+    window.open("https://www.albany.edu/search/search_results.php?cx=009452333206896616693%3Aabbjmkl5yry&cof=FORID%3A11&ie=UTF-8&sa.x=0&sa.y=0&sa=Search&siteurl=www.albany.edu%2F&ref=www.google.com%2F&ss=233j24671j6&q="+text_input);
+  }
+  }
+  
